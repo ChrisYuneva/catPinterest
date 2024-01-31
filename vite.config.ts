df39d9895188ef2342ef29580f16b19e53386303
ChrisYuneva <christina_yuneva@mail.ru>
2024-01-31 21:56:04 +0400

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/catPinterest/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: '/catPinterest/',
+    define: {
+      'process.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY)
+    },
+    plugins: [react()],
+  }
 })
